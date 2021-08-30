f4b6c3766e1672974303d759b694e31e61e78d75
tsuchan19991218 <ylwrvr.t.koga+git@gmail.com>
2021-08-30 09:23:56 +0900

    ファイルに処理順に番号を追加 make_region_label_table を編集中

---
+++ MyProject/data/2make_region_table.py
@@ -0,0 +1,87 @@
+import pandas as pd
+
+cell_line_list = ["GM12878"]
+
+def seq2sentence(k, stride, seq): # k-mer
+	# sentence 内の word 間は空白区切り
+
+	length = len(seq)
+	sentence = ""
+	start_pos = 0
+	while start_pos <= length-k:
+		# k-merに切る
+		word = seq[start_pos : start_pos + k]
+		
+		# 切り出したk-merを書き込む
+		sentence += word + ' '
+
+		start_pos += stride
+
+	return sentence
+
+
+def make_region_table(region_type):
+	# 入力に用いる塩基配列(enhancer, promoter, bin)についてのテーブルデータを作成します.
+	# 前提として、全領域の bedfile, fastafile が存在する必要があります.
+	# enhancer, promoter のテーブルデータに関する、行データの例
+		#	id				chr   	start	end		n_cnt	seq
+		#	ENHANCER_34		chr1	235686	235784	0		acgtcdgttcg...
+	
+	region_type = region_type.lower()
+	region_types = ["enhancer", "promoter", "bin"]
+	assert region_type in region_types, "enhancer, promoter, bin のいずれかを選択してください"
+
+	print(f"全ての {region_type} 領域について csvファイルを作成します.")
+	for cl in cell_line_list:
+		print(f"{cl} 開始")
+		bed_file = open("MyProject/data/bed/"+region_type+"/"+cl+"_"+region_type+"s.bed", "r")
+		fasta_file = open("MyProject/data/fasta/"+region_type+"/"+cl+"_"+region_type+"s.fa", "r")
+
+		id = 0
+		region_ids = [] # ENHANCER_0 などの id を入れていく
+		chrs = [] # chr1 などを入れていく
+		starts = []	# start pos を入れていく
+		ends = [] # end pos を入れていく
+		seqs = [] # 塩基配列 の sequence を入れていく
+		n_cnts = [] # sequence 内の "n" の個数を入れていく
+
+		bed_lines = bed_file.read().splitlines()
+		fasta_lines = fasta_file.read().splitlines()
+
+		for fasta_line in fasta_lines:
+
+			# ">chr1:17000-18000" のような行を飛ばす.
+			if fasta_line[0] == ">":
+				continue
+
+			bed_line_list = bed_lines[id].split("\t")
+			chrs.append(bed_line_list[0])
+			starts.append(bed_line_list[1])
+			ends.append(bed_line_list[2])
+			seqs.append(fasta_line)
+			n_cnt = fasta_line.count("n")
+			n_cnts.append(n_cnt)
+			region_id = region_type.upper() + "_" + str(id)
+			region_ids.append(region_id)
+			id += 1
+
+		df = pd.DataFrame({
+			"id":region_ids,
+			"chr":chrs,
+			"start":starts,
+			"end":ends,
+			"n_cnt":n_cnts,
+			"seq":seqs,
+		})
+		df.to_csv("MyProject/data/table/region/"+region_type+"/"+cl+"_"+region_type+"s.csv")
+
+		bed_file.close()
+		fasta_file.close()
+		print(f"{cl} 完了")
+
+	print(f"{region_type} 終了")
+
+make_region_table("enhancer")
+make_region_table("promoter")
+make_region_table("bin")
+
---
+++ MyProject/data/3make_region_label_table.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from tqdm import tqdm
+
+def make_region_label_table(cl, bin_length):
+	bin_table = pd.read_csv("MyProject/data/table/region/bin/GM12878_bins.csv", usecols=["id", "start", "end"])
+	print(bin_table.head())
+	print(f"周辺領域の幅: {bin_length}")
+	print(f"周辺領域の数: {len(bin_table)}")
+
+	enhancer_table = pd.read_csv("MyProject/data/table/region/enhancer/GM12878_enhancers.csv", usecols=["id", "start", "end"])
+	bin2enhancer_label = [""] * len(bin_table) # key: エンハンサーの番号, value: 周辺領域の番号
+
+	for _, data in tqdm(enhancer_table.iterrows()):
+		enhancer_id = data["id"]
+		enhancer_start = data["start"]
+		enhancer_end = data["end"]
+
+		bin_id = enhancer_start // bin_length
+		bin_start = bin_id * bin_length
+		bin_end = bin_start + bin_length
+		target_bin = -1 # 注目エンハンサーと最も重なっている領域番号の初期化
+		now_score = -1 # 注目エンハンサーとどれだけ重なっているかを示す指標
+		while bin_start < enhancer_end:
+			score = min(bin_end, enhancer_end) - max(bin_start, enhancer_start)
+			if now_score < score:
+				score = now_score
+				target_bin = bin_id
+			bin_id += 1
+			bin_start += bin_length
+			bin_end += bin_length
+		bin2enhancer_label[target_bin] += enhancer_id + ","
+
+	del enhancer_table
+	
+	promoter_table = pd.read_csv("MyProject/data/table/region/promoter/GM12878_promoters.csv", usecols=["id", "start", "end"])
+	bin2promoter_label = [""] * len(bin_table) # key: プロモーターの番号, value: 周辺領域の番号
+
+	for _, data in tqdm(promoter_table.iterrows()):
+		promoter_id = data["id"]
+		promoter_start = data["start"]
+		promoter_end = data["end"]
+		target_bin = -1 # 注目プロモーターと最も重なっている領域番号の初期化
+
+		bin_id = promoter_start // bin_length
+		bin_start = bin_id * bin_length
+		bin_end = bin_start + bin_length
+		now_score = -1 # 注目プロモーターとどれだけ重なっているかを示す指標
+		while bin_start < promoter_end:
+			score = min(bin_end, promoter_end) - max(bin_start, promoter_start)
+			if now_score < score:
+				score = now_score
+				target_bin = bin_id
+			bin_id += 1
+			bin_start += bin_length
+			bin_end += bin_length
+		bin2promoter_label[target_bin] += promoter_id + ","
+
+	del promoter_table
+
+	print(bin2enhancer_label[:10])
+	print(bin2promoter_label[:10])
+
+
+
+make_region_label_table("GM12878", 5000)
