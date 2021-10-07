697ca8a7ab4723483857e1594b2393e6204fac26
tsuchan19991218 <ylwrvr.t.koga+git@gmail.com>
2021-10-08 08:25:05 +0900

    ディレクトリ作成からパスを通す

--- MyProject/領域の延長実験/data_download.py
+++ MyProject/領域の延長実験/data_download.py
@@ -13,16 +13,19 @@ from sklearn.model_selection import StratifiedKFold, cross_validate
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
-def data_download(args, cell_line):
+def download_enhancer_and_promoter(args, cell_line):
 	# enhancer
-	print("エンハンサーをダウンロードします.")
-	os.system(f"wget {args.targetfinder_data_root_url}{cell_line}/output-ep/enhancers.bed -O {args.my_data_folder_path}bed/enhancer/{cell_line}_enhancers.bed")
+	print("エンハンサーをダウンロード...")
+	os.system(f"wget {args.targetfinder_data_root_url}/{cell_line}/output-ep/enhancers.bed -O {args.my_data_folder_path}/bed/enhancer/{cell_line}_enhancers.bed")
 
 	# promoter
-	print("プロモーターをダウンロードします.")
-	os.system(f"wget {args.targetfinder_data_root_url}{cell_line}/output-ep/promoters.bed -O {args.my_data_folder_path}bed/promoter/{cell_line}_promoters.bed")
+	print("プロモーターをダウンロード...")
+	os.system(f"wget {args.targetfinder_data_root_url}/{cell_line}/output-ep/promoters.bed -O {args.my_data_folder_path}/bed/promoter/{cell_line}_promoters.bed")
 
+	
+def download_reference_genome(args):
 	# reference genome
-	print("リファレンスゲノムをダウンロードします.")
-	os.system(f"wget {args.reference_genome_url} -O {args.my_data_folder_path}reference_genome/hg19.fa.gz")
-
+	print("リファレンスゲノムをダウンロード...")
+	os.system(f"wget {args.reference_genome_url} -O {args.my_data_folder_path}/reference_genome/hg19.fa.gz")
+	print("解凍...")
+	os.system(f"gunzip -f {args.my_data_folder_path}/reference_genome/hg19.fa.gz")
--- MyProject/領域の延長実験/main.py
+++ MyProject/領域の延長実験/main.py
@@ -14,7 +14,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
 # 自作関数
-from data_download import data_download
+from make_dirictory import make_directory
+import data_download
 from data_processing import data_processing
 import my_doc2vec
 import train_classifier
@@ -25,8 +26,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="エンハンサー, プロモーターの両端を延長したものに対し, doc2vecを行い,EPIs予測モデルの学習, 評価をする.")
 	parser.add_argument("--targetfinder_data_root_url", help="enhancer,promoterデータをダウンロードする際のtargetfinderのルートurl", default="https://github.com/shwhalen/targetfinder/raw/master/paper/targetfinder/")
 	parser.add_argument("--reference_genome_url", help="reference genome (hg19)をダウンロードする際のurl", default="https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/latest/hg19.fa.gz")
-	parser.add_argument("--cell_line_list", nargs="+", help="細胞株の名前 (複数選択可能)", default=["GM12878"])
-	parser.add_argument("-my_data_folder_path", help="データのルートとなるフォルダパス")
+	parser.add_argument("--cell_line_list", nargs="+", help="細胞株の名前 (複数選択可能)", default=["K562"])
 	parser.add_argument("--neighbor_length", default=5000)
 	parser.add_argument("-el", "--E_extended_left_length", type=int, default=0, help="エンハンサーの上流をどれだけ伸ばすか")
 	parser.add_argument("-er", "--E_extended_right_length", type=int, default=0, help="エンハンサーの下流をどれだけ伸ばすか")
@@ -35,21 +35,30 @@ if __name__ == '__main__':
 	parser.add_argument("--embedding_vector_dimention", type=int, default=100, help="paragraph vector の次元")
 	parser.add_argument("--k", type=int, default=6, help="doc2vec前処理のk-merのため")
 	parser.add_argument("--stride", type=int, default=1, help="doc2vec前処理のk-merのため")
-	parser.add_argument("--data_download", action="store_true", help="リファレンスゲノム, エンハンサー, プロモーター, を外部からダウンロードするか")
+	parser.add_argument("-my_data_folder_path", help="データのルートとなるフォルダパス")
+	parser.add_argument("--make_directory", action="store_true", help="実験に必要なディレクトリ構成を作る")
+	parser.add_argument("--download_reference_genome", action="store_true", help="リファレンスゲノムを外部からダウンロードするか")
 	parser.add_argument("--share_doc2vec", action="store_true", help="エンハンサーとプロモーターを一つのdoc2vecに共存させるか")
 	args = parser.parse_args()
 
-	pl_list = [0, 100, 500, 1000, 5000]
-	pr_list = [0, 100, 500, 1000, 5000]
-	el_list = [0]
-	er_list = [0]
+	# 必要なディレクトリの作成
+	if args.make_directory:
+		make_directory(args)
+
+	# リファレンスゲノムのダウンロード
+	if args.download_reference_genome:
+		data_download.download_reference_genome(args)
+
 
 	for cell_line in args.cell_line_list: # 細胞株毎のループ
 
-		# 必要なデータのダウンロード
-		if args.data_download:
-			data_download(args, cell_line)
+		# エンハンサープロモーターのダウンロード
+		data_download.download_enhancer_and_promoter(args, cell_line)
 
+		pl_list = [0]
+		pr_list = [0]
+		el_list = [0]
+		er_list = [0]
 		for (el, er, pl, pr) in list(itertools.product(el_list, er_list, pl_list, pr_list)): # 組み合わせ全列挙
 			args.E_extended_left_length = el
 			args.E_extended_right_length = er
