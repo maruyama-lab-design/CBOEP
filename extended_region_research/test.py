1f054edd8d5de257e3fb0ab148efd75e798920ce
tsuchan19991218 <ylwrvr.t.koga+git@gmail.com>
2021-12-16 15:36:49 +0900

    データ確認用ファイルを追加

--- extended_region_research/test.py
+++ extended_region_research/test.py
@@ -1,37 +1,8 @@
-from gensim import utils
-from gensim.models.doc2vec import TaggedDocument
-from gensim.models import Doc2Vec
-import pandas as pd
-import numpy as np
-import itertools
-
-import os
-import argparse
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-
-# classifier
-from sklearn.model_selection import StratifiedKFold, cross_validate
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-
-from t_sne import t_SNE
-
-import data_download
+import pickle
+from utils import pickle_load
 
 
-def test():
-	d2v_1 = os.path.join("/Users/ylwrvr/卒論/Koga_code/data/d2v/K562,el=0,er=0,pl=0,pr=10000,kmer=random,N=1.d2v")
-	d2v_5 = os.path.join("/Users/ylwrvr/卒論/Koga_code/data/d2v/K562,el=0,er=0,pl=0,pr=10000,kmer=random,N=5.d2v")
-	d2v_1_model = Doc2Vec.load(d2v_1)
-	d2v_5_model = Doc2Vec.load(d2v_5)
-
-	print(d2v_1_model.dv["enhancer_0"][:10])
-	print(d2v_5_model.dv["enhancer_0"][:10])
-
-	print(d2v_1_model.wv["aaa"][:10])
-	print(d2v_5_model.wv["aaa"][:10])
-
-	print(d2v_1_model.wv.index_to_key)
-
-test()
+result_dicts = pickle_load("/Users/ylwrvr/卒論/Koga_code/data/result/GM12878,d=100,way_of_kmer=normal,k=6,s=1,N=1,kmin=-1,kmax=-1,way_of_cv=random,clf=GBRT,trees=4000.pickle")
+for key, dic in result_dicts.items():
+	print(key)
+	print(dic["macro avg"]["f1-score"])
