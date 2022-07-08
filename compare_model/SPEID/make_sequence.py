2e51f478dba63cdb824213a102a5e7959ac2dd7c
tsuchan19991218 <ylwrvr.t.koga+git@gmail.com>
2022-07-08 19:30:09 +0900

    SPEIDにholdoutを追加

---
+++ compare_model/SPEID/make_sequence.py
@@ -0,0 +1,5 @@
+
+
+
+# SPEID の input は固定長なので
+# 固定長の enhancer/promoter を生成する．
---
+++ compare_model/SPEID/tmp.py
@@ -0,0 +1,7 @@
+
+#tensorflowをインポート
+import tensorflow
+
+#TensorFlowがGPUを認識しているか確認
+from tensorflow.python.client import device_lib
+device_lib.list_local_devices()
