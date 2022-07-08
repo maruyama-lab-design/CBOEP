1d9569bcdd19eea66630796d28b1ff20856b6c5f
tsuchan19991218 <ylwrvr.t.koga+git@gmail.com>
2022-07-08 20:44:03 +0900

    SPEID修正

--- compare_model/SPEID/train.py
+++ compare_model/SPEID/train.py
@@ -220,110 +220,107 @@ def hold_out(args):
 	valid_chroms = ["chr17", "chr18"]
 	test_chroms =  ["chr19", "chr20", "chr21", "chr22", "chrX"]
 
-	for fold, test_chrom in enumerate(test_chroms):
-		output_path = os.path.join(args.output_dir, f"fold_{fold+1}.csv")
-		print(f"fold {fold+1}...")
-		print(f"test chromosome : {test_chrom}")
-		test_chrom1, test_chrom2 = test_chrom[0], test_chrom[1]
+	output_path = os.path.join(args.output_dir, f"foldout.csv")
+	test_chrom1, test_chrom2 = test_chrom[0], test_chrom[1]
 
-		X_train_enh, X_valid_enh, X_test_enh = [], [], []
-		X_train_prm, X_valid_prm, X_test_prm = [], [], []
-		Y_train, Y_valid, Y_test = [], [], []
+	X_train_enh, X_valid_enh, X_test_enh = [], [], []
+	X_train_prm, X_valid_prm, X_test_prm = [], [], []
+	Y_train, Y_valid, Y_test = [], [], []
 
 
-		for chromosome in all_chroms:
-			if chromosome in test_chroms:
-				X_test_enh += chr2dataset[chromosome]["X_enhancers"]
-				X_test_prm += chr2dataset[chromosome]["X_promoters"]
-				Y_test += chr2dataset[chromosome]["labels"]
-			elif chromosome in train_chroms:
-				X_train_enh += chr2dataset[chromosome]["X_enhancers"]
-				X_train_prm += chr2dataset[chromosome]["X_promoters"]
-				Y_train += chr2dataset[chromosome]["labels"]
-			elif chromosome in valid_chroms:
-				X_valid_enh += chr2dataset[chromosome]["X_enhancers"]
-				X_valid_prm += chr2dataset[chromosome]["X_promoters"]
-				Y_valid += chr2dataset[chromosome]["labels"]
+	for chromosome in all_chroms:
+		if chromosome in test_chroms:
+			X_test_enh += chr2dataset[chromosome]["X_enhancers"]
+			X_test_prm += chr2dataset[chromosome]["X_promoters"]
+			Y_test += chr2dataset[chromosome]["labels"]
+		elif chromosome in train_chroms:
+			X_train_enh += chr2dataset[chromosome]["X_enhancers"]
+			X_train_prm += chr2dataset[chromosome]["X_promoters"]
+			Y_train += chr2dataset[chromosome]["labels"]
+		elif chromosome in valid_chroms:
+			X_valid_enh += chr2dataset[chromosome]["X_enhancers"]
+			X_valid_prm += chr2dataset[chromosome]["X_promoters"]
+			Y_valid += chr2dataset[chromosome]["labels"]
 
-		
-		X_train_enh = np.array(X_train_enh)
-		X_train_prm = np.array(X_train_prm)
-		Y_train = np.array(Y_train)
+	
+	X_train_enh = np.array(X_train_enh)
+	X_train_prm = np.array(X_train_prm)
+	Y_train = np.array(Y_train)
 
-		X_valid_enh = np.array(X_valid_enh)
-		X_valid_prm = np.array(X_valid_prm)
-		Y_valid = np.array(Y_valid)
+	X_valid_enh = np.array(X_valid_enh)
+	X_valid_prm = np.array(X_valid_prm)
+	Y_valid = np.array(Y_valid)
 
-		X_test_enh = np.array(X_test_enh)
-		X_test_prm = np.array(X_test_prm)
-		Y_test = np.array(Y_test)
+	X_test_enh = np.array(X_test_enh)
+	X_test_prm = np.array(X_test_prm)
+	Y_test = np.array(Y_test)
 
-		assert len(X_train_enh) == len(X_train_prm)
-		assert len(X_train_enh) == len(Y_train)
+	assert len(X_train_enh) == len(X_train_prm)
+	assert len(X_train_enh) == len(Y_train)
 
-		assert len(X_valid_enh) == len(X_valid_prm)
-		assert len(X_valid_enh) == len(Y_valid)
+	assert len(X_valid_enh) == len(X_valid_prm)
+	assert len(X_valid_enh) == len(Y_valid)
 
-		assert len(X_test_enh) == len(X_test_prm)
-		assert len(X_test_enh) == len(Y_test)
+	assert len(X_test_enh) == len(X_test_prm)
+	assert len(X_test_enh) == len(Y_test)
 
-		print(f"train enhancers sample size: {X_train_enh.shape}")
-		print(f"train promoters sample size: {X_train_prm.shape}")
+	print(f"train enhancers sample size: {X_train_enh.shape}")
+	print(f"train promoters sample size: {X_train_prm.shape}")
 
-		print(f"valid enhancers sample size: {X_valid_enh.shape}")
-		print(f"valid promoters sample size: {X_valid_prm.shape}")
+	print(f"valid enhancers sample size: {X_valid_enh.shape}")
+	print(f"valid promoters sample size: {X_valid_prm.shape}")
 
-		# type check
-		# print(type(X_train_enh))
-		# print(X_train_enh.shape)
-		# print(type(X_train_enh[0]))
-		# print(X_train_enh[0].shape)
+	# type check
+	# print(type(X_train_enh))
+	# print(X_train_enh.shape)
+	# print(type(X_train_enh[0]))
+	# print(X_train_enh[0].shape)
 
-		model = bm.build_model()
-		model.compile(
-			loss="binary_crossentropy",
-			optimizer=Adam(learning_rate=1e-5),
-		)
-		# print(model.summary())
+	model = bm.build_model()
+	model.compile(
+		loss="binary_crossentropy",
+		optimizer=Adam(learning_rate=1e-5),
+	)
+	# print(model.summary())
 
-		csv_logger = CSVLogger(os.path.join(args.output_dir, f"holdout_log"))
-		modelCheckpoint = ModelCheckpoint(		
-			filepath = os.path.join(os.path.dirname(__file__), "model", f"{args.cell_line}_holdout.h5"),
-			monitor='loss',
-			verbose=1,
-			save_best_only=True,
-			save_weights_only=False,
-			mode='min',
-			save_freq=1
-		)
-		earlystopper = EarlyStopping(monitor='val_loss', patience=50, verbose=1)
-		print("training...")
-		model.fit(
-			x=[X_train_enh, X_train_prm], 
-			y=[Y_train],
-			epochs=args.epochs,
-			batch_size=args.batch_size,
-			# class_weight={1: 20, 0:1},
-			callbacks=[modelCheckpoint, earlystopper, csv_logger],
-			validation_split=([X_valid_enh, X_valid_prm], [Y_valid])
-		)
+	csv_logger = CSVLogger(os.path.join(args.output_dir, f"holdout_log"))
+	modelCheckpoint = ModelCheckpoint(		
+		filepath = os.path.join(os.path.dirname(__file__), "model", "model-{epoch:02d}-{val_loss:.2f}.h5"),
+		monitor='val_loss',
+		verbose=1,
+		save_best_only=True,
+		save_weights_only=False,
+		mode='min',
+		save_freq=1
+	)
+	earlystopper = EarlyStopping(monitor='val_loss', patience=50, verbose=1)
+	print("training...")
+	history = model.fit(
+		x=[X_train_enh, X_train_prm], 
+		y=[Y_train],
+		epochs=args.epochs,
+		batch_size=args.batch_size,
+		# class_weight={1: 20, 0:1},
+		callbacks=[modelCheckpoint, earlystopper, csv_logger],
+		validation_data=([X_valid_enh, X_valid_prm], [Y_valid])
+	)
 
-		model.load_weights(os.path.join(os.path.dirname(__file__), "model", f"{args.cell_line}_holdout.h5"))
-		print("testing...")
-		y_pred = model.predict(
-			[X_test_enh, X_test_prm],
-			batch_size=args.batch_size,
-			verbose=1
-		)
+	model.load_weights(os.path.join(os.path.dirname(__file__), "model", "model-{epoch:02d}-{val_loss:.2f}.h5"))
+	print("testing...")
+	y_pred = model.predict(
+		[X_test_enh, X_test_prm],
+		batch_size=args.batch_size,
+		verbose=1
+	)
 
-		result_df = pd.DataFrame(
-			{
-				"y_test": Y_test,
-				"y_pred": np.reshape(y_pred, [-1])
-			},
-			index=None
-		)
-		result_df.to_csv(output_path)
+	result_df = pd.DataFrame(
+		{
+			"y_test": Y_test,
+			"y_pred": np.reshape(y_pred, [-1])
+		},
+		index=None
+	)
+	result_df.to_csv(output_path)
 
 
 
