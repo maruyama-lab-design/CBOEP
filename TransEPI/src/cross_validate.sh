075d029d0f87f3722e1d925b1c86ba12b930ef6f
DESKTOP-CH0O977\ylwrv <ylwrvr.t.koga+git@gmail.com>
2022-09-08 07:24:32 +0900

    config jsonの追加

--- TransEPI/src/cross_validate.sh
+++
@@ -1,4 +0,0 @@
-python cross_validate.py -c ./../config_dataset/balanced_BENGI.json --gpu 0 -o ./../output/balanced_BENGI
-python cross_validate.py -c ./../config_dataset/BENGI.json --gpu 0 -o ./../output/BENGI
-python cross_validate.py -c ./../config_dataset/maxflow.json --gpu 0 -o ./../output/maxflow
-python cross_validate.py -c ./../config_dataset/tolerance.json --gpu 0 -o ./../output/tolerance
